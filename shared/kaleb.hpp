ed20b9530b3eca8d405d37e092e4398e7abd73c3
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-11-16 18:31:17 +0100

    Make asset static

--- shared/kaleb.hpp
+++ shared/kaleb.hpp
@@ -6,12 +6,12 @@
 #include "KalebInfo.hpp"
 
 #define ASSET_REGISTRATOR(namespaze, name) \
-namespace { const ::Kaleb::AssetStore::AssetRegistrator name##_registrator (#namespaze "::" #name, &name); }
+namespace { static inline const ::Kaleb::AssetStore::AssetRegistrator name##_registrator (#namespaze "::" #name, &name); }
 
-#define DECLARE_FILE(_binary_id, namespaze, name)                       \
-namespace namespaze {                                                   \
-    extern "C" uint8_t _binary_id##_start[];                            \
-    extern "C" uint8_t _binary_id##_end[];                              \
-    ::Kaleb::Asset name {_binary_id##_start, _binary_id##_end};         \
-    ASSET_REGISTRATOR(namespaze, name);                                 \
+#define DECLARE_FILE(_binary_id, namespaze, name)                               \
+namespace namespaze {                                                           \
+    extern "C" uint8_t _binary_id##_start[];                                    \
+    extern "C" uint8_t _binary_id##_end[];                                      \
+    static inline ::Kaleb::Asset name {_binary_id##_start, _binary_id##_end};   \
+    ASSET_REGISTRATOR(namespaze, name);                                         \
 }
